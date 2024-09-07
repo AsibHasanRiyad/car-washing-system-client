ba6b28042092e63e99fc2f6bb515d5bcf9e307c4
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-09-07 17:02:47 +0600

    Working with navbar

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../shared/Navbar";
+import Container from "../ui/Container";
+
+const MainLayout = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default MainLayout;
