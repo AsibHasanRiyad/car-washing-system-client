b914f9e32a7e5a870939c909843debc7808a0a27
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-09-07 15:38:22 +0600

    Project initialization with shadcn and redux

---
+++ src/redux/features/serviceSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = "";
+export const serviceSlice = createSlice({
+  name: "service",
+  initialState,
+  reducers: {},
+});
+
+export default serviceSlice.reducer;
