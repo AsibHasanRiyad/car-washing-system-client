c9f9280db5c79b8e0c3cdad31faf3556ca260b50
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-09-10 11:00:33 +0600

    vision section removed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/shared/Hero";
 import Branch from "@/components/ui/Branch";
-import OurVision from "@/components/ui/OurVision";
+
 import PricePlan from "@/components/ui/PricePlan";
 import WhyUs from "@/components/ui/WhyUs";
 
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <Hero />
-      <OurVision />
+
       <PricePlan />
       <Branch />
       <WhyUs />
